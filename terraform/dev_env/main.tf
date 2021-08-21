11b9e62c4ff052389952af6db1523adcbbc02f60
ganesh321 <31589144+ganesh321@users.noreply.github.com>
2021-08-21 20:17:56 +0400

    changed config
    changed config

--- terraform/dev_env/main.tf
+++ terraform/dev_env/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 #    bucket = "yerramsettiveeraganeshdev"
 #    key = "terraform.tfstate"
 #    region = "us-east-1"
-#    access_key = "${var.aws_access_key}"
-#    secret_key = "${var.aws_secret_key}"
+#    access_key = "var.aws_access_key"
+#    secret_key = "var.aws_secret_key"
 #  }
 #}
